bc2486222a4b23d9b41b080ccab92a30d23eb50a
Nuraini Binti Rosli <18DDT19F1018@student.ptss.edu.my>
2021-04-20 21:13:28 +0800

    latest coding sub and function

--- Funtion Dan Sub/Form1.vb
+++ Funtion Dan Sub/Form1.vb
@@ -1,23 +1,39 @@
 ﻿Public Class Form1
-    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+    Private Sub SubMaklumat1()
         lbl1.Text = "Data 1"
     End Sub
 
-    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        SubMaklumat1()
+    End Sub
+
+    Private Sub SubMaklumat2()
         Dim strText1 As String
         strText1 = txt1.Text
         lbl2.Text = strText1
     End Sub
 
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        SubMaklumat2()
+    End Sub
+
+    Private Function FunMaklumat3()
+        Return "Data 2"
+    End Function
+
     Private Function btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
-        lbl3.Text = "Data 2"
-        Return lbl3
+        lbl3.Text = FunMaklumat3()
+    End Function
+
+    Private Function FunMaklumat4()
+        Dim strText2 As String
+        strText2 = txt2.Text
+        Return strText2
     End Function
 
     Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
-        Dim strText2 As String
-        strText2 = txt2.Text
-        lbl4.Text = strText2
-        Return lbl4
+        lbl4.Text = FunMaklumat4()
     End Function
+
 End Class
