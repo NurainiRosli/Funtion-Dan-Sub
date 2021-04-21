78bcee82dbefe59d6e18ffb0b3a3dc897f8b8c1e
Nuraini Binti Rosli <18DDT19F1018@student.ptss.edu.my>
2021-04-21 12:52:03 +0800

    update ByVal & ByRef

--- Funtion Dan Sub/Form1.vb
+++ Funtion Dan Sub/Form1.vb
@@ -1,5 +1,4 @@
 ﻿Public Class Form1
-
     Private Sub SubMaklumat1()
         lbl1.Text = "Data 1"
     End Sub
@@ -8,15 +7,17 @@
         SubMaklumat1()
     End Sub
 
-    Private Sub SubMaklumat2()
-        Dim strText1 As String
-        strText1 = txt1.Text
-        lbl2.Text = strText1
+
+    Private Sub SubMaklumat2(ByRef strText1 As String)
+        strText1 = " Hi " & strText1
     End Sub
 
     Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
-        SubMaklumat2()
+        Dim strText1 As String = txt1.Text
+        SubMaklumat2(strText1)
+        lbl2.Text = strText1
     End Sub
+
 
     Private Function FunMaklumat3()
         Return "Data 2"
@@ -26,14 +27,13 @@
         lbl3.Text = FunMaklumat3()
     End Function
 
-    Private Function FunMaklumat4()
-        Dim strText2 As String
-        strText2 = txt2.Text
+
+    Private Function FunMaklumat4(ByVal strText2 As String)
         Return strText2
     End Function
 
     Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
-        lbl4.Text = FunMaklumat4()
+        lbl4.Text = FunMaklumat4(txt2.Text)
     End Function
 
 End Class
